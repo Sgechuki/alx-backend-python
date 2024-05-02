90a178da121a20666537613fe940022b08f1da40
Sgechuki <sgechuki@outlook.com>
2024-05-02 22:37:10 +0000

    Doc string length

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 from typing import List
 """
-This module holds the
-sum_list function
+This module holds the sum_list function
 """
 
 
 def sum_list(input_list: List[float]) -> float:
     """
-    type-annotated function sum_list which
     takes a list input_list of floats as
     argument and returns their sum as a float
     """
