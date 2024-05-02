3e4bcde1aa385b4f768f916d2b4cc0db0f7de953
Sgechuki <sgechuki@outlook.com>
2024-05-02 22:42:00 +0000

    Doc string length

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,7 +7,6 @@ Task 5: Complex types - list of floats
 
 def sum_list(input_list: List[float]) -> float:
     """
-    takes a list input_list of floats as
-    argument and returns their sum as a float
+    Compute the sum of  a list of floats
     """
     return float(sum(input_list))
