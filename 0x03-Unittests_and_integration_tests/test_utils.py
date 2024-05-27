c17c3b1aeec8add9363dfea67496d59866d52ead
Sgechuki <sgechuki@outlook.com>
2024-05-27 19:02:58 +0000

    Add return type

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,6 +19,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
     def test_access_nested_map(self, nested_map: Dict,
-                               path Tuple[str], output: Union[Dict, int]):
+                               path Tuple[str],
+                               output: Union[Dict, int]) -> None:
         """test that the method returns what it is supposed to"""
         self.assertEqual(access_nested_map(nested_map, path), output)
