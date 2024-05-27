e81b95317f71d7cc5f9722bda9f9d607497e9b9a
Sgechuki <sgechuki@outlook.com>
2024-05-27 19:26:58 +0000

    Raise error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,7 +19,22 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
     def test_access_nested_map(self, nested_map: Dict,
-                               path Tuple[str],
+                               path: Tuple[str],
                                output: Union[Dict, int]) -> None:
-        """test that the method returns what it is supposed to"""
+        """
+        test that the method returns what it is supposed to
+        """
         self.assertEqual(access_nested_map(nested_map, path), output)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+        ])
+    def test_access_nested_map_exception(self,
+                                         nested_map: Dict,
+                                         path: Tuple[str],
+                                         exception: Exception) -> None:
+        """
+        test that a KeyError is raised
+        """
+        self.assertRaises(exception, access_nested_map(nested_map, path))
